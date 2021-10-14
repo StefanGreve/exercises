7ac42378bbfaededf3deea0f2b7ab66ced9024fd
StefanGreve <greve.stefan@outlook.jp>
2021-10-14 11:16:46 +0200

    Implement exercise 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // treetop
+        for (int i = 6; i >= 0; i--) {
+            System.out.print(new String(new char[i]).replace("\0", " "));
+            System.out.println(new String(new char[-2 * i + 13]).replace("\0", "*"));
+        }
+        // stem
+        for (int i = 0; i < 3; i++) {
+            System.out.printf("%8s\n", "***");
+        }
     }
 }
