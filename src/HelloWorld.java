e5d0cde669751e83ca1fd203b2e381c2d2f992d3
StefanGreve <greve.stefan@outlook.jp>
2021-10-14 11:15:09 +0200

    Implement exercise 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Das ist ein Beispielsatz.\nEin Beispielsatz ist das.");
     }
 }
