47b595353b58dad4d3748afab62036847e7e24e9
StefanGreve <greve.stefan@outlook.jp>
2021-10-14 15:18:38 +0200

    Implement exercise 5

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+
 public class HelloWorld {
+    public static float fahrenheitToCelsius(int fahrenheit) {
+        return (fahrenheit - 32F) * 5 / 9;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        ArrayList<Integer> temperatures = new ArrayList<Integer>() {
+            {
+                add(-20);
+                add(-10);
+                add(0);
+                add(20);
+                add(30);
+            }
+        };
+        System.out.println("Fahrenheit  |   Celsius");
+        System.out.println("------------|----------");
+
+        for (int temp : temperatures) {
+            System.out.printf("%-12d|%10.2f\n", temp, fahrenheitToCelsius(temp));
+        }
+
     }
 }
