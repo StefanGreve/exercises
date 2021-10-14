4ab69bc1b1d4badd52c0ef548b959745319d1a4c
StefanGreve <greve.stefan@outlook.jp>
2021-10-14 15:37:10 +0200

    Implement exercise 6

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.printf("%.2f\n", 22.4234234);
+        System.out.printf("%.2f\n", 111.2222);
+        System.out.printf("%.2f\n", 4.0);
+        System.out.printf("%.2f\n", 1000000.551);
+        System.out.printf("%.2f\n", 97.34);
     }
 }
