fafd31e8bac090ba429afad8a934527cbaceda8f
StefanGreve <greve.stefan@outlook.jp>
2021-10-27 10:25:10 +0200

    Implement solution to exercise 2, week 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("What's your name? ");
+            String name = scanner.nextLine();
+
+            System.out.print("How old are you? ");
+            int age = scanner.nextInt();
+
+            System.out.printf("You're %s and %d years old.", name, age);
+        }
     }
 }
