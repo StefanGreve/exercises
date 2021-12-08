737e3d69a8d6e4bc17d45247e6e7bc75cd5027c2
StefanGreve <greve.stefan@outlook.jp>
2021-12-08 09:21:48 +0100

    Implement exercise 12 (problem 4)

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,24 @@
 public class HelloWorld {
+    public static double getCubeVolume(double a) {
+        return Math.pow(a, 3);
+    }
+
+    public static double getCuboidVolume(double a, double b, double c) {
+        return a * b * c;
+    }
+
+    public static double getPyramidVolume(double a, double h) {
+        return Math.pow(a, 2) * h / 3;
+    }
+
+    public static double getSphereVolume(double r) {
+        return 4 / 3 * Math.pow(r, 3) * Math.PI;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(getCubeVolume(2));
+        System.out.println(getCuboidVolume(2, 2, 2));
+        System.out.println(getPyramidVolume(2, 5));
+        System.out.println(getSphereVolume(4));
     }
 }
