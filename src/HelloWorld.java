12811633736d9273d62c5c55537578046dce7385
StefanGreve <greve.stefan@outlook.jp>
2022-01-06 08:00:11 +0100

    Filter even numbers in range of [0,19]

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
+import java.util.stream.IntStream;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        IntStream.range(0, 19).filter(x -> x % 2 == 1).forEach(System.out::println);
     }
 }
