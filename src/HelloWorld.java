09a54ec3728879480ae955f19b351afd51f5b427
StefanGreve <greve.stefan@outlook.jp>
2022-01-10 12:06:15 +0100

    Implement draw-a-square exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,21 @@
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.Collections;
+
 public class HelloWorld {
+    public static String repeat(String string, int k) {
+        return String.join("", Collections.nCopies(k, string));
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Size: ");
+            int size = scanner.nextInt();
+
+            //@formatter:off
+            IntStream.range(0, size).forEach(i -> System.out.printf("%s%n", (i == 0 || i == size - 1) ? repeat("* ", size) : String.join("", new String[]{"*", repeat(" ", 2 * size - 3), "*"})));
+            //@formatter:on
+        }
     }
 }
