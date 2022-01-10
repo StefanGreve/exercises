7edef71bd02b594ccad53404af1315e03de7ee1f
StefanGreve <greve.stefan@outlook.jp>
2022-01-10 11:44:00 +0100

    Implement flight of stairs exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.Collections;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Height: ");
+            int height = scanner.nextInt();
+            System.out.print("Width: ");
+            int width = scanner.nextInt();
+
+            //@formatter:off
+            IntStream.range(0, height).forEach(h -> System.out.printf("%" + width * height + "s%n", String.join("", Collections.nCopies(width * (h + 1), "*"))));
+            //@formatter:on
+        }
     }
 }
