e597e5107c91acc74cfc63f5b79e21d166da783a
StefanGreve <greve.stefan@outlook.jp>
2022-03-26 00:12:35 +0100

    Update Database class
    * implement ClearTable
    * implement DropTable
    * change return type for LoadModal (List >> IEnumerable)
    * TODO: hard-code connection string until I figure out how to
      share a single configuration file between multiple projects

--- Galaxy/Galaxy/Database.cs
+++ Galaxy/Galaxy/Database.cs
@@ -14,20 +14,33 @@ namespace Galaxy
 	{
 		private static string LoadConnectionString(string name = "Default")
 		{
-			return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+			// TODO: create shared configuration file
+			//return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+			return @"Data Source=.\InformationSystem.db;Version=3;";
 		}
 
-		public static List<T> LoadModel<T>(string table)
+		public static IEnumerable<T> LoadModel<T>(string table)
 		{
 			using IDbConnection connection = new SQLiteConnection(LoadConnectionString());
-			var output = connection.Query<T>($"SELECT * FROM {table}", new DynamicParameters());
-			return output.ToList();
+			return connection.Query<T>($"SELECT * FROM {table}", new DynamicParameters());
 		}
 
 		public static void InsertModel<T>(T model, string table, string[] properties)
 		{ 
 			using IDbConnection connection = new SQLiteConnection(LoadConnectionString());
 			connection.Execute($"INSERT INTO {table} ({string.Join(",", properties)}) values ({string.Join(",", properties.Select(p => $"@{p}"))})", model);
+		}
+
+		public static void ClearTable(string table)
+		{
+			using IDbConnection connection = new SQLiteConnection(LoadConnectionString());
+			connection.Execute($"DELETE FROM {table}");
+		}
+
+		public static void DropTable(string table)
+		{
+			using IDbConnection connection = new SQLiteConnection(LoadConnectionString());
+			connection.Execute($"DROP TABLE {table}");
 		}
 	}
 }
