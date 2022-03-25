7e31892ede280bd3458848996cdc3131cb43f49b
StefanGreve <greve.stefan@outlook.jp>
2022-03-26 00:16:24 +0100

    Implement Attack and refactor class a little

--- Galaxy/Galaxy/Spaceship.cs
+++ Galaxy/Galaxy/Spaceship.cs
@@ -20,7 +20,7 @@ namespace Galaxy
 
 		public double LifeSupportShield { get; set; } = 100;
 
-		public double Shell { get; set; } = 100;
+		public double Shell { get { return (ProtectiveShield + LifeSupportShield) / 2; } }
 
 		public int Hits { get; set; } = 0;
 
@@ -37,17 +37,39 @@ namespace Galaxy
 
 		public void SendMessage(string message)
 		{
-			Database.InsertModel<Message>(new(message, Name), "Message", new[] { "Content", "Sender", "Created" });
+			Database.InsertModel<Message>(new(message, Name), table: "Message", properties: new[] { "Content", "Sender", "Created" });
 		}
+
+		public IEnumerable<Message> ReceiveMessages() => Database.LoadModel<Message>(table: "Message");
 
 		public List<string> GetLogBook()
 		{
 			throw new NotImplementedException();
 		}
 
-		public void Attack(Spaceship target, Weapon weapon)
+		public void Attack(Spaceship target, int index)
 		{
-			throw new NotImplementedException();
+			Random random = new Random();
+
+			try
+			{
+				Weapon weapon = Weapons[index];
+
+				if (weapon.Count > 0)
+				{
+					if (random.Next(0, 101) <= weapon.Accuracy)
+					{
+						target.ProtectiveShield -= weapon.Damage;
+						target.LifeSupportShield -= weapon.PiercingDamage ? weapon.Damage : 0;
+						target.Hits++;
+					}
+					weapon.Count--;
+				}
+			}
+			catch (IndexOutOfRangeException exception)
+			{
+				Console.Error.WriteLine(exception.Message);
+			}
 		}
 
 		public void RequestRepair(bool powerSupply, bool protectiveShield, bool lifeSupportShield, int androidCount)
